57f81f82ccd4d36c13915bd865b7b4f374ad6e07
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-26 03:40:35 +0900

    ✨ [feat]: cors 허용 설정

--- socket/socket.js
+++ socket/socket.js
@@ -2,7 +2,13 @@ const socketIo = require("socket.io");
 const jwt = require("jsonwebtoken");
 
 function initializeSocket(server) {
-  const io = socketIo(server);
+  const io = socketIo(server, {
+    cors: {
+      origin: "http://localhost:3000", // localhost:3000 cors 허용
+      methods: ["*"], // 모든 메소드 허용
+      credentials: true,
+    },
+  });
 
   io.on("connection", (socket) => {
     console.log("SOCKET CONNECTED: ", socket.id);
