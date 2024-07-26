97bd4feb3a575aa2f7b531e85d062ecc2269ee7e
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-27 01:20:29 +0900

    🐛 [Fix]: cors 설정 추가

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const io = initializeSocket(server);
 
 const routes = require("./app/routers/index")(io); // 라우터에 io 객체 전달
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://socket.gamegoo.co.kr"], // localhost:3000 cors 허용
+    methods: ["*"], // 모든 메소드 허용
+    credentials: true, // 쿠키 허용
+  })
+);
+
 app.use("/", routes);
 
 server.listen(3000, () => {
