122e49926a7710ad7b7f51b610f12beb76d433aa
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-27 00:10:17 +0900

    ✨ [Feat] jwt 포함하는 socket 연결 구현

--- socket/socket.js
+++ socket/socket.js
@@ -1,5 +1,13 @@
 const socketIo = require("socket.io");
 const jwt = require("jsonwebtoken");
+const { secretKey } = require("../config/config");
+
+const initChat = require("./handlers/chat/chatInit");
+const initAlarm = require("./handlers/alarm/alarmInit");
+const initMatching = require("./handlers/matching/matchingInit");
+const initFriend = require("./handlers/friend/friendInit");
+
+const { emitMemberInfo } = require("./emitters/memberEmitter");
 
 function initializeSocket(server) {
   const io = socketIo(server, {
@@ -11,7 +19,36 @@ function initializeSocket(server) {
   });
 
   io.on("connection", (socket) => {
-    console.log("SOCKET CONNECTED: ", socket.id);
+    if (!socket.memberId) {
+      console.log("a user connected, memberId:", socket.memberId, "socketId:", socket.id);
+    }
+
+    // socket auth에서 JWT 토큰 추출
+    const token = socket.handshake.auth.token;
+
+    if (token) {
+      // (#2-2) jwt 토큰 검증 및 socket 바인딩
+      jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+          console.log("Invalid token");
+        } else {
+          socket.memberId = decoded.id; // 해당 소켓 객체에 memberId 추가
+          socket.token = token; // 해당 소켓 객체에 token 추가
+          console.log("a user connected, memberId:", socket.memberId, "socketId:", socket.id);
+
+          // (#2-3) "member-info" event emit
+          emitMemberInfo(socket);
+
+          // (#2-5) 초기화 함수들 호출
+          initChat(socket, io);
+          initAlarm(socket, io);
+          initMatching(socket, io);
+          initFriend(socket, io);
+        }
+      });
+    } else {
+      console.log("No token provided, 비로그인 사용자의 socket connection 입니다.");
+    }
 
     socket.on("disconnect", () => {
       console.log("SOCKET DISCONNECTED: ", socket.id);
