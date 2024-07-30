b63f8ad66fbe774f1d58d43b7b294dc8fa60c294
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-31 01:16:31 +0900

    ✨ [Feat] 채팅방 입장 api 요청 및 렌더링

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -97,3 +97,22 @@ async function getChatroomListApi() {
     console.error("Error:", error);
   }
 }
+
+async function enterChatroomApi(chatroomUuid) {
+  try {
+    const jwtToken = localStorage.getItem("jwtToken");
+    const response = await fetch(`${API_SERVER_URL}/v1/member/chat/${chatroomUuid}`, {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`, // Include JWT token in header
+      },
+    });
+    const data = await response.json();
+    if (data.isSuccess && data.result) {
+      return data.result;
+    } else {
+      throw new Error("enterChatroomApi failed");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
