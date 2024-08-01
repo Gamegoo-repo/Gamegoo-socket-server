dbe5c218c3d76f0eede1cb446869e20b1e7e061a
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-08-01 13:12:52 +0900

    ✨ [Feat] 클라이언트 chat-message event listener 구현

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -116,3 +116,22 @@ async function enterChatroomApi(chatroomUuid) {
     console.error("Error:", error);
   }
 }
+
+async function readChatApi(chatroomUuid, timestamp) {
+  try {
+    const jwtToken = localStorage.getItem("jwtToken");
+    const response = await fetch(`${API_SERVER_URL}/v1/chat/${chatroomUuid}/read?timestamp=${timestamp}`, {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`, // Include JWT token in header
+      },
+    });
+    const data = await response.json();
+    if (data.isSuccess && data.result) {
+      return data.result;
+    } else {
+      throw new Error("readChatApi failed");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
