17380c2d67091808908c1a131d63fe43635ac0b3
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-08-03 03:26:03 +0900

    ♻️ [Refactor] 3000 -> 8080 api 요청 시 에러 처리 추가

--- socket/apis/chatApi.js
+++ socket/apis/chatApi.js
@@ -11,42 +11,56 @@ const API_SERVER_URL = config.apiServerUrl;
  * @returns
  */
 async function fetchChatroomUuid(socket) {
-  const response = await axios.get(`${API_SERVER_URL}/v1/member/chatroom/uuid`, {
-    headers: {
-      Authorization: `Bearer ${socket.token}`, // Include JWT token in header
-    },
-  });
-  if (response.data.isSuccess) {
-    return response.data.result;
-  } else {
-    if (["JWT400", "JWT401", "JWT404"].includes(response.data.code)) {
-      console.error("JWT token Error: ", response.data.message);
-      throw new JWTTokenError(`JWT token Error: ${response.data.message}`, response.data.code);
+  try {
+    const response = await axios.get(`${API_SERVER_URL}/v1/member/chatroom/uuid`, {
+      headers: {
+        Authorization: `Bearer ${socket.token}`, // Include JWT token in header
+      },
+    });
+    if (response.data.isSuccess) {
+      return response.data.result;
     }
-    console.error("Failed to fetch chatroom uuid: ", response.data.message);
-    throw new Error(`Failed to fetch chatroom uuid: ${response.data.message}`);
+  } catch (error) {
+    if (error.response && error.response.data) {
+      const data = error.response.data;
+      if (["JWT400", "JWT401", "JWT404"].includes(data.code)) {
+        console.error("JWT token Error: ", data.message);
+        throw new JWTTokenError(`JWT token Error: ${data.message}`, data.code);
+      }
+      console.error("Failed GET chatroom uuid: ", data.message);
+      throw new Error(`Failed GET chatroom uuid: ${data.message}`);
+    } else {
+      throw new Error(`Request failed: ${error.message}`);
+    }
   }
 }
 
 async function postChatMessage(socket, chatroomUuid, message) {
-  const response = await axios.post(
-    `${API_SERVER_URL}/v1/chat/${chatroomUuid}`,
-    { message: message },
-    {
-      headers: {
-        Authorization: `Bearer ${socket.token}`,
-      },
+  try {
+    const response = await axios.post(
+      `${API_SERVER_URL}/v1/chat/${chatroomUuid}`,
+      { message: message },
+      {
+        headers: {
+          Authorization: `Bearer ${socket.token}`,
+        },
+      }
+    );
+    if (response.data.isSuccess) {
+      return response.data.result;
     }
-  );
-  if (response.data.isSuccess) {
-    return response.data.result;
-  } else {
-    if (["JWT400", "JWT401", "JWT404"].includes(response.data.code)) {
-      console.error("JWT token Error: ", response.data.message);
-      throw new JWTTokenError(`JWT token Error: ${response.data.message}`, response.data.code);
+  } catch (error) {
+    if (error.response && error.response.data) {
+      const data = error.response.data;
+      if (["JWT400", "JWT401", "JWT404"].includes(data.code)) {
+        console.error("JWT token Error: ", data.message);
+        throw new JWTTokenError(`JWT token Error: ${data.message}`, data.code);
+      }
+      console.error("Failed POST chat message: ", data.message);
+      throw new Error(`Failed POST chat message: ${data.message}`);
+    } else {
+      throw new Error(`Request failed: ${error.message}`);
     }
-    console.error("Failed to fetch chatroom uuid: ", response.data.message);
-    throw new Error(`Failed to fetch chatroom uuid: ${response.data.message}`);
   }
 }
 
--- socket/apis/friendApi.js
+++ socket/apis/friendApi.js
@@ -10,20 +10,27 @@ const API_SERVER_URL = config.apiServerUrl;
  * @returns
  */
 async function fetchFriends(socket) {
-  const response = await axios.get(`${API_SERVER_URL}/v1/member/friends`, {
-    headers: {
-      Authorization: `Bearer ${socket.token}`, // Include JWT token in header
-    },
-  });
-  if (response.data.isSuccess) {
-    return response.data.result;
-  } else {
-    if (["JWT400", "JWT401", "JWT404"].includes(response.data.code)) {
-      console.error("JWT token Error: ", response.data.message);
-      throw new JWTTokenError(`JWT token Error: ${response.data.message}`, response.data.code);
+  try {
+    const response = await axios.get(`${API_SERVER_URL}/v1/member/friends`, {
+      headers: {
+        Authorization: `Bearer ${socket.token}`, // Include JWT token in header
+      },
+    });
+    if (response.data.isSuccess) {
+      return response.data.result;
     }
-    console.error("Failed to fetch friend list: ", response.data.message);
-    throw new Error(`Failed to fetch friend list: ${response.data.message}`);
+  } catch (error) {
+    if (error.response && error.response.data) {
+      const data = error.response.data;
+      if (["JWT400", "JWT401", "JWT404"].includes(data.code)) {
+        console.error("JWT token Error: ", data.message);
+        throw new JWTTokenError(`JWT token Error: ${data.message}`, data.code);
+      }
+      console.error("Failed GET friend list: ", data.message);
+      throw new Error(`Failed GET friend list: ${data.message}`);
+    } else {
+      throw new Error(`Request failed: ${error.message}`);
+    }
   }
 }
 
