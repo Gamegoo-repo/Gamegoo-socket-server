cf7a8cb5a33bece8a1f4666951251a8b7113b646
Rimi <lora3226@inu.ac.kr>
2024-08-13 21:26:58 +0900

    ✨ [Feat] 수정된 매칭 로직 반영

--- socket/handlers/matching/matchingHandler.js
+++ socket/handlers/matching/matchingHandler.js
@@ -11,17 +11,20 @@ function setupMatchListeners(socket, io) {
 
   // 사용자가 매칭을 시작할 때 
   socket.on("matching_started", (request) => {
+    const memberId = socket.memberId;
     const matchingType = request.matchingType;
     const gameMode = request.gameMode;
     const mike = request.mike;
-    const tier = request.tier;
-    const rank = request.rank;
-    const manner = request.manner;
     const mainP = request.mainP;
     const subP = request.subP;
     const wantP = request.wantP;
+    const gameStyle1 = request.gameStyle1;
+    const gameStyle2 = request.gameStyle2;
+    const gameStyle3 = request.gameStyle3;
 
-    printMatchingInformation(matchingType,mike,tier,rank,mainP,mainP,subP,wantP,manner,gameMode)
+    // 디버깅
+    console.log("memberId : "+ memberId);
+    printMatchingInformation(matchingType,mike,mainP,subP,wantP,gameMode,gameStyle1,gameStyle2,gameStyle3);
 
     // 사용자 소켓을 gameMode에 해당하는 룸에 조인시킴
     socket.join("GAMEMODE_"+gameMode);
@@ -48,15 +51,6 @@ function setupMatchListeners(socket, io) {
   // 4. 매칭이 실패했을 때
   socket.on("matching_failed",(request)=>{
     console.log(request);
-  });
-
-  // 사용자가 접속을 끊었을 때
-  socket.on("disconnect", () => {
-    const room = userRooms[socket.id];
-    if (room) {
-      console.log(`User ${socket.id} left room: ${room}`);
-      delete userRooms[socket.id]; // 사용자 정보 삭제
-    }
   });
 }
 
@@ -85,18 +79,17 @@ function getUsersInRoom(io, room) {
  * @param {*} manner 
  * @param {*} gameMode 
  */
-function printMatchingInformation(matchingType, mike, tier, rank, manner, mainP, subP, wantP,manner,gameMode){
+function printMatchingInformation(matchingType, mike, mainP, subP, wantP, gameMode, gameStyle1,gameStyle2,gameStyle3){
   console.log('========== Received Matching Information ==========');
   console.log('Matching Type: ' + matchingType);
   console.log('Game Mode: ' + gameMode);
   console.log('Mike: ' + mike);
-  console.log('Tier: ' + tier);
-  console.log('Rank: ' + rank);
-  console.log('Manner: ' + manner);
+  console.log('GameStyle1: ' + gameStyle1);
+  console.log('GameStyle2: ' + gameStyle2);
+  console.log('GameStyle3: ' + gameStyle3);
   console.log('Main P: ' + mainP);
   console.log('Sub P: ' + subP);
   console.log('Want P: ' + wantP);
-  console.log('manner: ' + manner);
   console.log('gameMode: ' + gameMode);
   console.log('==========================================');
 }
