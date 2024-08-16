4221ec5e3d15f55f0006fd0933eab253858ffad2
Rimi <lora3226@inu.ac.kr>
2024-08-16 16:24:36 +0900

    ✨ [Feat] 매칭 API 불러오기 및 priority 객체에 넣기

--- socket/handlers/matching/matchingInitializer.js
+++ socket/handlers/matching/matchingInitializer.js
@@ -1,3 +1,5 @@
+const PriorityQueue = require('priorityqueuejs');
+
 /**
  * 로그인 했을 때 매칭관련 부분 초기화 
  * @param {*} socket 
@@ -5,7 +7,9 @@
  * @returns 
  */
 function initializeMatching(socket, io) {
-  // 로그인 했을 때 매칭에서 초기 설정할 부분은 아직 없음.
+  // 우선순위 큐 설정
+  socket.priorityTable = {};
+
   return;
 }
 
