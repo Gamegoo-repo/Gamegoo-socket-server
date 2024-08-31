f1595fc26b109d8efbb00972f21fb47cbdaafcd0
Rimi <lora3226@inu.ac.kr>
2024-08-31 13:55:52 +0900

    feat: matching_not_found 로직 추가

--- socket/handlers/matching/matchingHandler/matchingFoundHandler.js
+++ socket/handlers/matching/matchingHandler/matchingFoundHandler.js
@@ -37,4 +37,37 @@ function deleteSocketFromMatching(socket, io, otherSocket, roomName) {
   otherSocket.highestPriorityNode = null;
 }
 
-module.exports = { deleteSocketFromMatching };
+/**
+ * 소켓 자신만을 매칭 풀에서 삭제 (room leave, priorityTree 제거 및 초기화)
+ * @param {*} socket
+ * @param {*} otherSocket
+ * @param {*} roomName
+ */
+function deleteMySocketFromMatching(socket, io, roomName) {
+  // 14) 소켓 룸에서 제거
+  socket.leave(roomName);
+
+  // 15) priorityTree에서 삭제
+  const room = io.sockets.adapter.rooms.get(roomName);
+
+  if (room) {
+    // 룸에 있는 각 소켓에 대해 콜백 함수 실행
+    room.forEach((socketId) => {
+      const roomSocket = io.sockets.sockets.get(socketId);
+      if (roomSocket) {
+        // roomSocket의 priorityTree에서 socket의 값을 지우기
+        roomSocket.priorityTree.removeByMemberId(socket.memberId);
+      }
+    });
+  } else {
+    console.log(`Room ${roomName} does not exist or is empty.`);
+  }
+
+  // 16) 각자의 priorityTree 삭제
+  socket.priorityTree.clear();
+
+  // 각자의 highestPriorityNode 삭제
+  socket.highestPriorityNode = null;
+}
+
+module.exports = { deleteSocketFromMatching, deleteMySocketFromMatching };
