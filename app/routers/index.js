b16458b6e30407cc4bea31948c349a24443f2a0c
Rimi <lora3226@inu.ac.kr>
2024-09-24 00:39:50 +0900

    ✨[Feat] 시스템 메세지 매칭 인원 전체 조회

--- app/routers/index.js
+++ app/routers/index.js
@@ -8,6 +8,7 @@ module.exports = (io) => {
   const { login } = require("../controller/loginController")(io); // io 객체를 전달
   const { logout } = require("../controller/logoutController")(io);
   const { socketRoomJoin, emitSystemMessage } = require("../controller/socketController")(io);
+  const { countUsersInMatching, countUsersInMatchingByTier } = require("../controller/systemMessageController")(io);
 
   router.use("/img", express.static(path.join(__dirname, "../../public/img")));
 
@@ -22,6 +23,12 @@ module.exports = (io) => {
 
   // 8080 -> 3000 으로 보낼 api. 특정 memberId를 갖는 socket 모두에게 시스템 메시지 event emit
   router.post("/socket/sysmessage", emitSystemMessage);
+
+  // 시스템 메시지 1. 지금 몇 명이 매칭을 기다리고 있는지
+  router.get('/socket/message',countUsersInMatching);
+
+  // 시스템 메시지 2. 나와 같은 티어의 n 명이 매칭 중인지 -> 룸에 속한 티어를 돌면서 나랑 같은 애를 찾고,
+  router.get('/socket/message/tier',countUsersInMatchingByTier);
 
   return router;
 };
