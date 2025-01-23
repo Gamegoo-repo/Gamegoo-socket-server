582252c963a8c54fb1c8e866ef41bf2b05653791
Eunjin3395 <jennyeunjin@khu.ac.kr>
2025-01-23 22:52:59 +0900

    ♻️ [Refactor] api 컨트롤러 코드 로그 리팩토링

--- app/controller/logoutController.js
+++ app/controller/logoutController.js
@@ -1,15 +1,15 @@
-const { successResponse, failResponse } = require("../common/responseFormatter");
-const logger = require("../../common/winston.js");
+const { successResponse, failResponse } = require("../common/responseFormatter.js");
+const { logger } = require("../../common/winston.js");
 
 function logout(io) {
   return (req, res) => {
     // "Socket-Id" 헤더에서 클라이언트 소켓 ID 추출
     const socketId = req.headers["socket-id"];
-    logger.http("=== Received 'logout' request", `socketId:${socketId} ===`);
+    logger.info(`[POST] /logout  |  IP: ${req.ip} | Logout Request`, { socketId: socketId });
 
     // Socket-Id 존재 여부 검증
     if (!socketId) {
-      logger.error("=== Logout failed: Missing socket ID in headers ===");
+      logger.error(`[POST] /logout  |  IP: ${req.ip} | Logout failed: Missing socket ID in headers`);
       return res.status(401).json(failResponse("SOCKET_INIT_FAILED", "header에서 socket id를 추출할 수 없습니다."));
     }
 
@@ -17,15 +17,16 @@ function logout(io) {
 
     if (socket) {
       // (#7-4) 소켓 연결 종료
-      logger.info("Disconnecting socket", `socketId:${socketId}, memberId:${socket.memberId}`);
       socket.disconnect(true);
+      logger.info(`[POST] /logout  |  IP: ${req.ip} | Socket Disconnected`, { socketId: socket.id, memberId: socket.memberId });
     } else {
-      logger.error("=== Logout failed: Socket not found", `socketId:${socketId} ===`);
+      logger.error(`[POST] /logout  |  IP: ${req.ip} | Logout failed: Socket not found`, { socketId: socketId });
       return res.status(404).json(failResponse("SOCKET_NOT_FOUND", "socket id에 해당하는 소켓을 찾을 수 없습니다."));
     }
 
     // (#7-5) return 200
-    logger.info("=== Logout successful", `socketId:${socketId}, memberId:${socket.memberId} ===`);
+    logger.info(`[POST] /logout  |  IP: ${req.ip} | Logout Success`, { socketId: socketId });
+
     return res.status(200).json(successResponse("로그아웃 성공"));
   };
 }
--- app/routers/index.js
+++ app/routers/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { join } = require("node:path");
 const path = require("path");
 
 module.exports = (io) => {
@@ -8,7 +7,7 @@ module.exports = (io) => {
   const { login } = require("../controller/loginController")(io); // io 객체를 전달
   const { logout } = require("../controller/logoutController")(io);
   const { socketRoomJoin, emitSystemMessage } = require("../controller/socketController")(io);
-  const { countUsersInMatching, countUsersInMatchingByTier } = require("../controller/systemMessageController")(io);
+  const { countUsersInMatching } = require("../controller/systemMessageController")(io);
 
   router.use("/img", express.static(path.join(__dirname, "../../public/img")));
 
@@ -19,16 +18,15 @@ module.exports = (io) => {
   router.post("/logout", logout);
 
   // (#10-5) 8080 -> 3000 으로 보낼 api. 특정 socket을 특정 chatroom에 join시키기
-  router.post("/socket/room/join", socketRoomJoin);
+  router.post("/internal/socket/room/join", socketRoomJoin);
 
   // 8080 -> 3000 으로 보낼 api. 특정 memberId를 갖는 socket 모두에게 시스템 메시지 event emit
-  router.post("/socket/sysmessage", emitSystemMessage);
+  router.post("/internal/socket/sysmessage", emitSystemMessage);
 
   // 시스템 메시지 : 지금 몇 명이 매칭을 기다리고 있는지
   // 엔드포인트 뒤에 query로 tier 붙이면 각 티어마다 몇 명 있는지 조회
-  // ex) /socket/message?tier=BRONZE 
-  router.get('/socket/message',countUsersInMatching);
-
+  // ex) /socket/message?tier=BRONZE
+  router.get("/socket/message", countUsersInMatching);
 
   return router;
 };
